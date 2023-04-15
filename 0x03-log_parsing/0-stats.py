2baa5f6b089d35654584b9fe34ba8fe3c4da9c8c
Adams Adjartey <kaadjartey@gmail.com>
2023-04-15 01:02:06 +0000

    quick fix

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,39 +1,39 @@
 #!/usr/bin/python3
-'''a script that reads stdin line by line and computes metrics'''
+""" A script that reads stdin line by line and computes metrics """
 
 import sys
 
 
-if __name__ == "__main__":
-   i = 0
-   status = {
-       '200': 0,
-       '301': 0,
-       '400': 0,
-       '401': 0,
-       '403': 0,
-       '404': 0,
-       '405': 0,
-       '500': 0
-    }
-    fileSize = 0
-    def printstats(fileSize, status):
-        """ Method to print """
-        print("File size: {:d}".format(fileSize))
-        for key in sorted(status.keys()):
-            if status[key] != 0:
-               print("{}: {:d}".format(key, status[key]))
-    try:
-        for line in sys.stdin:
-            x = line.split()
-            if len(x) >= 2:
-                if words[-2] in status.keys():
-                    status[x[-2]] += 1
-            fileSize += int(x[-1])
-            i += 1
-            if not i % 10:
-                printstats(fileSize, status)
-         printstats(fileSize, status)
-    except KeyboardInterrupt:
-        printstats(fileSize, status)
-        raise
+def print_status(dict, size):
+    """Print the format"""
+    print("File size: {}".format(size))
+    for key in sorted(dict.keys()):
+        if dict[key] != 0:
+            print("{}: {}".format(key, dict[key]))
+
+status_dict = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0,
+               '404': 0, '405': 0, '500': 0}
+
+file_size = 0
+count = 0
+
+try:
+    for line in sys.stdin:
+        if count != 0 and count % 10 == 0:
+            print_status(status_dict, file_size)
+        emp = line.split(" ")
+        count += 1
+
+        try:
+            file_size += int(emp[-1])
+        except:
+            pass
+        try:
+            if emp[-2] in status_dict.keys():
+                status_dict[el[-2]] += 1
+                                                                                                                                              except:
+            pass
+                                                                                                                                          print_status(status_dict, file_size)
+except KeyboardInterrupt:
+    print_status(status_dict, file_size)
+    raise
